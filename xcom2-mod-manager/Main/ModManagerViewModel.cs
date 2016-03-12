f696b280dfa503ef78b9ec02c28c8fc42800df6c
Sergey Skrynnikov <minotaar.hh@gmail.com>
2016-03-12 23:59:39 +0300

    PropertyChanged for path

--- xcom2-mod-manager/Main/ModManagerViewModel.cs
+++ xcom2-mod-manager/Main/ModManagerViewModel.cs
@@ -1,27 +1,34 @@
-﻿using System;
+﻿using PropertyChanged;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Forms;
 using System.Windows.Input;
 using xcom2_mod_manager.View.ViewModel;
 
 namespace XCom2ModManager.Main
 {
+    [ImplementPropertyChanged]
     public class ModManagerViewModel
     {
         private RelayCommand launchClickCommand;
+        private RelayCommand setXcomFolderCommand;
+        private string xcomPath = "E:\\";
 
         public string Title
         {
             get { return "XCOM2 Mod Manager"; }
         }
 
-        private void LaunchClick(object sender)
+        public string XcomPath
         {
-            MessageBox.Show("Launch click");
+            get { return xcomPath; }
+            set { xcomPath = value; }
         }
+    
 
         public ICommand LaunchClickCommand
         {
@@ -34,5 +41,33 @@ namespace XCom2ModManager.Main
                 return launchClickCommand;
             }
         }
+
+        public ICommand SetXcomFolderCommand
+        {
+            get
+            {
+                if (setXcomFolderCommand == null)
+                {
+                    setXcomFolderCommand = new RelayCommand(s => SetXcomFolder(s));
+                }
+                return setXcomFolderCommand;
+            }
+        }
+
+        private void SetXcomFolder(object sender)
+        {
+            var dialog = new FolderBrowserDialog();
+            var result = dialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                XcomPath = dialog.SelectedPath;
+            }
+        }
+
+        private void LaunchClick(object sender)
+        {
+            System.Windows.MessageBox.Show("Launch click");
+        }
+
     }
 }
